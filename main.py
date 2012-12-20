45100aef3ffeeb8a1a37ef51fefd3670da82aa2b
Nir Naor <nirnaori@gmail.com>
2012-12-20 12:24:07 +0200

    some more refactoring due to the code review

--- main.py
+++ main.py
@@ -8,20 +8,25 @@ def print_menu():
     print '2. Toad'
     print '3. Beacon'
 
+
+def draw_selected_pattern_forever(choice):
+        if (choice == 1):
+            Patterns.blinker().draw_matrix_forever()
+        elif (choice == 2):
+            Patterns.toad().draw_matrix_forever()
+        elif (choice == 3):
+            Patterns.beacon().draw_matrix_forever()
+        else:
+            raise ValueError("there is no suitable pattern for the given number")
+
+
 if __name__ == '__main__':
     try:
         print_menu()
-        choice = raw_input()
-
-        if (choice == '1'):
-            Patterns.blinker().draw_matrix_forever()
-        elif (choice == '2'):
-            Patterns.toad().draw_matrix_forever()
-        elif (choice == '3'):
-            Patterns.beacon().draw_matrix_forever()
-        else:
-            print('bye...')
-            sys.exit()
+        choice = int(raw_input())
+        draw_selected_pattern_forever(choice)
+        print('bye...')
+        sys.exit()
     except KeyboardInterrupt:
         print('bye...')
         sys.exit()
